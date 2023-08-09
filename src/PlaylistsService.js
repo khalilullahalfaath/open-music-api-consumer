1425347d16369b42d8a422fba9b4b4c44798afe0
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-08-09 11:48:35 +0700

    first commit

---
+++ src/PlaylistsService.js
@@ -0,0 +1,32 @@
+const {Pool} = require('pg');
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getSongsFromPlaylist(userId, playlistId) {
+    const playlistWithUserQuery = {
+      text: 'SELECT playlists.*, users.username FROM playlists LEFT JOIN users ON users.id = playlists.owner WHERE playlists.id = $1',
+      values: [playlistId],
+    };
+
+    const playlistWithUserResult = await this._pool.query(playlistWithUserQuery);
+
+    const songsQuery = {
+      text: 'SELECT songs.id, songs.title, songs.performer FROM songs LEFT JOIN playlist_songs ON playlist_songs.song_id = songs.id WHERE playlist_songs.playlist_id = $1',
+      values: [playlistId],
+    };
+
+    const playlist = playlistWithUserResult.rows[0];
+    const resultSongsQuery = await this._pool.query(songsQuery);
+
+    return {
+      id: playlist.id,
+      name: playlist.name,
+      songs: resultSongsQuery.rows,
+    };
+  }
+}
+
+module.exports = PlaylistsService;
